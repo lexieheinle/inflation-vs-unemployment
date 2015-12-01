bc2211d23bb714390f0edf71331fc58f3d78e916
alexlucke <alex.m.lucke@gmail.com>
2015-12-01 15:13:35 -0600

    test push

--- inflation/scripts/scrapInflation.py
+++ inflation/scripts/scrapInflation.py
@@ -11,4 +11,6 @@ for tr in soup.find_all('tr')[1:]:
      for header in tr.find_all('th'):
       tds = tr.find_all('td')
       print (header.text, tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text,
-             tds[5].text, tds[6].text, tds[7].text, tds[8].text, tds[9].text, tds[10].text, tds[11].text, tds[12].text,)
+             tds[5].text, tds[6].text, tds[7].text, tds[8].text, tds[9].text, tds[10].text, tds[11].text, tds[12].text)
+
+print ("hi")
