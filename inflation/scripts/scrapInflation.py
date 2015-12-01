9237f8d2a9cc28b1f9219d0f98a7df0b0426e601
alexlucke <alex.m.lucke@gmail.com>
2015-12-01 15:55:44 -0600

    working with scrape code and django

--- inflation/scripts/scrapInflation.py
+++ inflation/scripts/scrapInflation.py
@@ -1,6 +1,14 @@
-#!/usr/bin/python
-import urllib.request
-from bs4 import BeautifulSoup
+from reports.models import Inflation
+
+try:
+    from BeautifulSoup import BeautifulSoup
+except:
+    from bs4 import BeautifulSoup
+    
+import urllib, urllib2, string, datetime, time, re
+from django.template.defaultfilters import slugify, urlize
+
+#html = urllib2.urlopen("http://www.usinflationcalculator.com/inflation/historical-inflation-rates/" )
 
 url = "http://www.usinflationcalculator.com/inflation/historical-inflation-rates/" 
 page = urllib.request.urlopen(url).read()
@@ -10,7 +18,4 @@ soup = BeautifulSoup(page, "html.parser")
 for tr in soup.find_all('tr')[1:]:
      for header in tr.find_all('th'):
       tds = tr.find_all('td')
-      print (header.text, tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text,
-             tds[5].text, tds[6].text, tds[7].text, tds[8].text, tds[9].text, tds[10].text, tds[11].text, tds[12].text)
-
-print ("hi")
+      print (header.text, tds[0].text, tds[1].text, tds[2].text, tds[3].text, tds[4].text, tds[5].text, tds[6].text, tds[7].text, tds[8].text, tds[9].text, tds[10].text, tds[11].text, tds[12].text) 
