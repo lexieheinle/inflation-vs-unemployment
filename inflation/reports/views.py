806abe232b4e027c1486ca41a8ceb40e57778f6b
lexieheinle <lexie.heinle@gmail.com>
2015-12-03 16:05:22 -0600

    Adding templates

--- inflation/reports/views.py
+++ inflation/reports/views.py
@@ -1,11 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.http import HttpResponse
 from reports.models import Inflation, Unemployment, Interest
 from django.views.generic import ListView
 
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render_to_response('main.html')
+  
+def inflationOverview(request):
+    stat = "Inflation"
+    objects = Inflation
+    dictionaries = {'name': stat}
+    return render_to_response('stat.html')
 
 def inflation(request, year, month):
     return HttpResponse("Hello, world. You're {} at the {} year on {}.".format("inflation", year, month))
