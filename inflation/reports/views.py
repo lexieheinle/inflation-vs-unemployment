33d7d7bf0095462d0d80344bc9e024d405bd7114
lexieheinle <lexie.heinle@gmail.com>
2015-12-04 17:33:36 -0600

    Elegant dict for unemployment json

--- inflation/reports/views.py
+++ inflation/reports/views.py
@@ -15,11 +15,13 @@ def unemploymentData():
 def unemploymentOverview(request):
     stat = "Unemployment"
     objects = Unemployment.objects.all()
-   # url = unemploymentData()
-    temp_list = []
+    myList = []
     for object in Unemployment.objects.all().values():
-      temp_list.append([[str(object['date'])], [object['rate']]])
-    json_data = json.dumps(temp_list)
+      littleDict = {}
+      littleDict['date'] = str(object['date'])
+      littleDict['rate'] = object['rate']
+      myList.append(littleDict)
+    json_data = json.dumps(myList)
     dictionaries = {'name': stat, 'objects': objects, 'json_data': json_data}
     return render_to_response('stat.html', dictionaries)
 
