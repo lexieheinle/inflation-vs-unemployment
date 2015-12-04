0650162849e9275fc07c024d07d95d5d5ef9aed0
lexieheinle <lexie.heinle@gmail.com>
2015-12-04 17:27:07 -0600

    Add Tony's crazy, ugly but still helpful data

--- inflation/reports/views.py
+++ inflation/reports/views.py
@@ -2,21 +2,25 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse
 from reports.models import Inflation, Unemployment, Interest
 from django.views.generic import ListView
-
+import json
 
 # Create your views here.
 def index(request):
     return render_to_response('main.html')
   
-def unemploymentData(request):
+def unemploymentData():
   data = list(Unemployment.objects.all().values())
   return JsonResponse(data, safe=False)
   
 def unemploymentOverview(request):
     stat = "Unemployment"
     objects = Unemployment.objects.all()
-    url = unemploymentData()
-    dictionaries = {'name': stat, 'objects': objects, 'dataUrl': url }
+   # url = unemploymentData()
+    temp_list = []
+    for object in Unemployment.objects.all().values():
+      temp_list.append([[str(object['date'])], [object['rate']]])
+    json_data = json.dumps(temp_list)
+    dictionaries = {'name': stat, 'objects': objects, 'json_data': json_data}
     return render_to_response('stat.html', dictionaries)
 
 def inflation(request, year, month):
