4b2308773ac83343d29ca875c52ee2e2671f1e16
Lexie Heinle <lexie.heinle@gmail.com>
2015-12-10 13:47:07 -0600

    Add documentation for models' classes

--- inflation/reports/models.py
+++ inflation/reports/models.py
@@ -3,32 +3,35 @@ from datetime import datetime
 
 # Create your models here.
 class Inflation(models.Model):
-  rate = models.FloatField(null=True)
-  date = models.DateField()
+    """Model class to create Inflation objects with rate, date"""
+    rate = models.FloatField(null=True)
+    date = models.DateField()
   
-  def __str__(self):
-    return "{0} on {1}".format(self.rate, self.date)
+    def __str__(self):
+        return "{0} on {1}".format(self.rate, self.date)
   
-  def get_absolute_url(self):
-    return "/inflation/{0}/".format(datetime.strftime(self.date, "%Y/%m"))
+    def get_absolute_url(self):
+        return "/inflation/{0}/".format(datetime.strftime(self.date, "%Y/%m"))
 
 
 class Unemployment(models.Model):
-  rate = models.FloatField(null=True)
-  date = models.DateField()
+    """Model class to create Unemployment objects with rate, date"""
+    rate = models.FloatField(null=True)
+    date = models.DateField()
   
-  def __str__(self):
-    return "{0} on {1}".format(self.rate, self.date)
+    def __str__(self):
+        return "{0} on {1}".format(self.rate, self.date)
   
-  def get_absolute_url(self):
-    return "/unemployment/{0}".format(datetime.strftime(self.date, "%Y/%m"))
+    def get_absolute_url(self):
+        return "/unemployment/{0}".format(datetime.strftime(self.date, "%Y/%m"))
 
 class Interest(models.Model):
-  rate = models.FloatField(null=True)
-  date = models.DateField()
+    """Model class to create Interest objects with rate, date"""
+    rate = models.FloatField(null=True)
+    date = models.DateField()
   
-  def __str__(self):
-    return "{0} on {1}".format(self.rate, self.date)
+    def __str__(self):
+        return "{0} on {1}".format(self.rate, self.date)
   
-  def get_absolute_url(self):
-    return "/interest/{0}".format(datetime.strftime(self.date, "%Y/%m"))
+    def get_absolute_url(self):
+        return "/interest/{0}".format(datetime.strftime(self.date, "%Y/%m"))
