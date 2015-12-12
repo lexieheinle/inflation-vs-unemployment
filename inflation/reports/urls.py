afe78be9facfd637b43f9b9cdfa2c9bd58477716
lexieheinle <lexie.heinle@gmail.com>
2015-12-11 20:08:23 -0600

    Bake out files

--- inflation/reports/urls.py
+++ inflation/reports/urls.py
@@ -8,7 +8,11 @@ urlpatterns = [
   url(r'^unemployment$', views.Unemployment_overview.as_view(), name='unemploymentOver'),
   url(r'^interest$', views.Interest_overview.as_view(), name="interestOver"),
   url(r'^sources$', views.Source.as_view(), name='source'),
-  url(r'^decade/(?P<decade_num>[0-9]{4})/$', views.Time_spans.as_view(), name='decade_num'),
+  url(r'^decade/1970/$', views.Time_spans1970.as_view(), name='1970s'),
+  url(r'^decade/1980/$', views.Time_spans1980.as_view(), name='1980s'),
+  url(r'^decade/1990/$', views.Time_spans1990.as_view(), name='1990s'),
+  url(r'^decade/2000/$', views.Time_spans2000.as_view(), name='2000s'),
+  url(r'^decade/2010/$', views.Time_spans2010.as_view(), name='2010s'),
 ]
 
 
--- inflation/reports/views.py
+++ inflation/reports/views.py
@@ -8,11 +8,13 @@ import datetime
 
 # Create your views here.
 class Index(BuildableTemplateView):
+    build_path = "index.html"
     stat = 'Index'
     queryset = {'name': stat}
     template_name = 'main.html'
     
 class Source(BuildableTemplateView):
+    build_path = 'source.html'
     stat = 'Source'
     queryset = {'name': stat}
     template_name = 'source.html'
@@ -20,6 +22,7 @@ class Source(BuildableTemplateView):
 class Unemployment_overview(BuildableListView):
     template_name = "stat.html"
     queryset = []
+    build_path = 'reports/unemployment.html'
     def get_context_data(self):
         my_list = []
         for object in Unemployment.objects.all().values():
@@ -35,6 +38,7 @@ class Unemployment_overview(BuildableListView):
 class Interest_overview(BuildableListView):
     template_name = "stat.html"
     queryset = []
+    build_path = 'reports/interest.html'
     def get_context_data(self):
         my_list = []
         for object in Interest.objects.all().values():
@@ -50,6 +54,7 @@ class Interest_overview(BuildableListView):
 class Inflation_overview(BuildableListView):
     template_name = "stat.html"
     queryset = []
+    build_path = 'reports/inflation.html'
     def get_context_data(self):
         my_list = []
         for object in Inflation.objects.all().values():
@@ -62,11 +67,12 @@ class Inflation_overview(BuildableListView):
         context['name'] = 'Inflation'
         context['json_data'] = json_data
         return context
-class Time_spans(BuildableListView):
+class Time_spans1970(BuildableListView):
     template_name = "time.html"
     queryset = []
-    def get_context_data(self, **kwargs):
-        decade_num = self.kwargs['decade_num']
+    build_path = 'reports/decade/1970s.html'
+    def get_context_data(self):
+        decade_num = '1970'
         decade_num = int(decade_num)
         stat = "{0}s".format(decade_num)
         start_date = datetime.date(decade_num, 1, 1)
@@ -92,7 +98,155 @@ class Time_spans(BuildableListView):
             little_dict['rate'] = object['rate']
             int_list.append(little_dict)
         json_data_int = json.dumps(int_list)
-        context = super(Time_spans, self).get_context_data(**kwargs)
+        context = super(Time_spans1970, self).get_context_data()
+        context['name'] = stat
+        context['json_data_infl'] = json_data_infl
+        context['json_data_un'] = json_data_un
+        context['json_data_int'] = json_data_int
+        return context
+class Time_spans1980(BuildableListView):
+    template_name = "time.html"
+    queryset = []
+    build_path = 'reports/decade/1980s.html'
+    def get_context_data(self):
+        decade_num = '1980'
+        decade_num = int(decade_num)
+        stat = "{0}s".format(decade_num)
+        start_date = datetime.date(decade_num, 1, 1)
+        end_date = datetime.date(decade_num + 10, 1, 1)
+        infl_list = []
+        for object in Inflation.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            infl_list.append(little_dict)
+        json_data_infl = json.dumps(infl_list)
+        un_list = []
+        for object in Unemployment.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            un_list.append(little_dict)
+        json_data_un = json.dumps(un_list)
+        int_list = []
+        for object in Interest.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            int_list.append(little_dict)
+        json_data_int = json.dumps(int_list)
+        context = super(Time_spans1980, self).get_context_data()
+        context['name'] = stat
+        context['json_data_infl'] = json_data_infl
+        context['json_data_un'] = json_data_un
+        context['json_data_int'] = json_data_int
+        return context
+class Time_spans1990(BuildableListView):
+    template_name = "time.html"
+    queryset = []
+    build_path = 'reports/decade/1990s.html'
+    def get_context_data(self):
+        decade_num = '1990'
+        decade_num = int(decade_num)
+        stat = "{0}s".format(decade_num)
+        start_date = datetime.date(decade_num, 1, 1)
+        end_date = datetime.date(decade_num + 10, 1, 1)
+        infl_list = []
+        for object in Inflation.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            infl_list.append(little_dict)
+        json_data_infl = json.dumps(infl_list)
+        un_list = []
+        for object in Unemployment.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            un_list.append(little_dict)
+        json_data_un = json.dumps(un_list)
+        int_list = []
+        for object in Interest.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            int_list.append(little_dict)
+        json_data_int = json.dumps(int_list)
+        context = super(Time_spans1990, self).get_context_data()
+        context['name'] = stat
+        context['json_data_infl'] = json_data_infl
+        context['json_data_un'] = json_data_un
+        context['json_data_int'] = json_data_int
+        return context
+class Time_spans2000(BuildableListView):
+    template_name = "time.html"
+    queryset = []
+    build_path = 'reports/decade/2000s.html'
+    def get_context_data(self):
+        decade_num = '2000'
+        decade_num = int(decade_num)
+        stat = "{0}s".format(decade_num)
+        start_date = datetime.date(decade_num, 1, 1)
+        end_date = datetime.date(decade_num + 10, 1, 1)
+        infl_list = []
+        for object in Inflation.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            infl_list.append(little_dict)
+        json_data_infl = json.dumps(infl_list)
+        un_list = []
+        for object in Unemployment.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            un_list.append(little_dict)
+        json_data_un = json.dumps(un_list)
+        int_list = []
+        for object in Interest.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            int_list.append(little_dict)
+        json_data_int = json.dumps(int_list)
+        context = super(Time_spans2000, self).get_context_data()
+        context['name'] = stat
+        context['json_data_infl'] = json_data_infl
+        context['json_data_un'] = json_data_un
+        context['json_data_int'] = json_data_int
+        return context
+class Time_spans2010(BuildableListView):
+    template_name = "time.html"
+    queryset = []
+    build_path = 'reports/decade/2010s.html'
+    def get_context_data(self):
+        decade_num = '2010'
+        decade_num = int(decade_num)
+        stat = "{0}s".format(decade_num)
+        start_date = datetime.date(decade_num, 1, 1)
+        end_date = datetime.date(decade_num + 10, 1, 1)
+        infl_list = []
+        for object in Inflation.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            infl_list.append(little_dict)
+        json_data_infl = json.dumps(infl_list)
+        un_list = []
+        for object in Unemployment.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            un_list.append(little_dict)
+        json_data_un = json.dumps(un_list)
+        int_list = []
+        for object in Interest.objects.filter(date__range = [start_date, end_date]).values():
+            little_dict = {}
+            little_dict['date'] = str(object['date'])
+            little_dict['rate'] = object['rate']
+            int_list.append(little_dict)
+        json_data_int = json.dumps(int_list)
+        context = super(Time_spans2010, self).get_context_data()
         context['name'] = stat
         context['json_data_infl'] = json_data_infl
         context['json_data_un'] = json_data_un
